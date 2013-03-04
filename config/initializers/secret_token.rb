92fede4f4ac4fb11f1dec8bd22ed9ac5944f468b
Jeff Cohen <jeff@codeacademy.org>
2013-03-04 13:52:58 -0600

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordbot::Application.config.secret_token = '63e7587e21882d2807bd45152bdbae822277ebb2e7668b52b69a91fcde3d8822de128407ce19b67f52f88d0bc5b98c99bc2da650b5315af300bd514d39e042d5'
